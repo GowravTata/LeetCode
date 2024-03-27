35c2e1fde8550a956750a65005ceed9a48e36ab4
Gowrav Tata <54976985+GowravTata@users.noreply.github.com>
2024-03-27 12:22:09 +0000

    Added solved leetcode problems

---
+++ 30 Days of Pandas/1484. Group Sold Products By The Date.py
@@ -0,0 +1,71 @@
+"""
+Table Activities:
+
++-------------+---------+
+| Column Name | Type    |
++-------------+---------+
+| sell_date   | date    |
+| product     | varchar |
++-------------+---------+
+There is no primary key (column with unique values) for this table. It may contain duplicates.
+Each row of this table contains the product name and the date it was sold in a market.
+ 
+
+Write a solution to find for each date the number of different products sold and their names.
+
+The sold products names for each date should be sorted lexicographically.
+
+Return the result table ordered by sell_date.
+
+The result format is in the following example.
+
+ 
+
+Example 1:
+
+Input: 
+Activities table:
++------------+------------+
+| sell_date  | product     |
++------------+------------+
+| 2020-05-30 | Headphone  |
+| 2020-06-01 | Pencil     |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | Basketball |
+| 2020-06-01 | Bible      |
+| 2020-06-02 | Mask       |
+| 2020-05-30 | T-Shirt    |
++------------+------------+
+Output: 
++------------+----------+------------------------------+
+| sell_date  | num_sold | products                     |
++------------+----------+------------------------------+
+| 2020-05-30 | 3        | Basketball,Headphone,T-shirt |
+| 2020-06-01 | 2        | Bible,Pencil                 |
+| 2020-06-02 | 1        | Mask                         |
++------------+----------+------------------------------+
+Explanation: 
+For 2020-05-30, Sold items were (Headphone, Basketball, T-shirt), we sort them lexicographically and separate them by a comma.
+For 2020-06-01, Sold items were (Pencil, Bible), we sort them lexicographically and separate them by a comma.
+For 2020-06-02, the Sold item is (Mask), we just return it.
+"""
+import pandas as pd
+
+data = [['2020-05-30', 'Headphone'], ['2020-06-01', 'Pencil'], ['2020-06-02', 'Mask'], ['2020-05-30', 'Basketball'], ['2020-06-01', 'Bible'], ['2020-06-02', 'Mask'], ['2020-05-30', 'T-Shirt']]
+activities = pd.DataFrame(data, columns=['sell_date', 'product']).astype({'sell_date':'datetime64[ns]', 'product':'object'})
+
+# 1st Solution
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    a=activities.groupby(['sell_date'])['product'].nunique().reset_index().rename(columns={'product':'num_sold'})
+    b=activities.groupby(['sell_date'])['product'].unique().reset_index().rename(columns={'product':'products'})
+    abc = pd.merge(a,b,on='sell_date')
+    abc['products'] = abc['products'].apply(lambda x:",".join(sorted(x)))
+    return abc
+
+# Efficient Solution
+def categorize_products(activities: pd.DataFrame) -> pd.DataFrame:
+    stats = activities.groupby(by='sell_date').agg(
+        num_sold = ('product','nunique'),
+        products = ('product', lambda x: ','.join(sorted(set(x))))
+                ).reset_index()
+    return stats
